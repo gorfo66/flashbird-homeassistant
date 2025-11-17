79dff1eade8a450071f9cc777e6c003c98a3f19f
Romain <frog@gorfo.com>
2025-11-17 20:50:19 +0000

    fix lint task

--- custom_components/flashbird/lock.py
+++ custom_components/flashbird/lock.py
@@ -16,7 +16,6 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
 ) -> None:
-    """Do the setup all the lock entities."""
     _LOGGER.debug("Calling async_setup_entry entry=%s", entry.entry_id)
 
     async_add_entities(
